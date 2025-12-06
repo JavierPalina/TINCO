c0f50802cbedb208c08c0ae31dddc444429d87f2
Javier Palina <javiariel2003@live.com.ar>
2025-12-05 22:10:45 -0300

    changes 3

--- src/components/ui/SignaturePad.tsx
+++ src/components/ui/SignaturePad.tsx
@@ -43,7 +43,6 @@ export function SignaturePad({ value, onChange, disabled }: Props) {
         penColor="black"
         canvasProps={{ className: 'w-full h-full' }}
         onEnd={handleEnd}
-        disabled={disabled}
       />
       {!disabled && (
         <Button
